e7664b2cad4f6dff91433aade5295979641cf0e6
saraytin <77133961+saraytin@users.noreply.github.com>
2021-01-28 22:13:45 -0600

    Edit from Sam

--- src/FabriceMain.java
+++ src/FabriceMain.java
@@ -1,5 +1,6 @@
 public class FabriceMain {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Fabrice.");
+        System.out.println("Hello Fabrice, my name is Sam.");
     }
 }
